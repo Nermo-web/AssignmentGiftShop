875527b6aea4c6d0a50d3124ab742577adfa110d
Nermin Mitkov <nermin.mitkov1998@gmail.com>
2020-11-25 22:11:53 +0200

    share code on github

---
+++ frontend/src/data.js
@@ -0,0 +1,78 @@
+const data= {
+    products:[
+        {
+            _id:'1',
+            name:'Polo Smart T-shirt',
+            categort:'Shirts',
+            image:'/images/p1.jpg',
+            price:19.99,
+            countInStock: 10,
+            brand:'Polo',
+            rating:4.5,
+            numReviews: 10,
+            description:'High quality product'
+        },
+        {
+            _id:'2',
+            name:'Nike Smart T-shirt',
+            categort:'Shirts',
+            image:'/images/p2.jpg',
+            price:42.99,
+            countInStock: 12,
+            brand:'Nike',
+            rating:4.0,
+            numReviews: 10,
+            description:'High quality product'
+        },
+        {
+            _id:'3',
+            name:'Puma Smart T-shirt',
+            categort:'Pants',
+            image:'/images/p3.jpg',
+            price:12.99,
+            countInStock: 19,
+            brand:'Puma',
+            rating:4.7,
+            numReviews: 22,
+            description:'High quality product'
+        },
+        {
+            _id:'4',
+            name:'Adidas Fit Trausers',
+            categort:'Pants',
+            image:'/images/p4.jpg',
+            price:69,
+            countInStock: 20,
+            brand:'Adidas',
+            rating:4.5,
+            numReviews: 10,
+            description:'High quality product'
+        },
+        {
+            _id:'5',
+            name:'Lacoste Slim Trausers',
+            categort:'Pants',
+            image:'/images/p5.jpg',
+            price:65,
+            countInStock: 60,
+            brand:'Lacoste',
+            rating:4.3,
+            numReviews: 15,
+            description:'High quality product'
+        },
+        {
+            _id:'6',
+            name:'Polo Fit Trausers',
+            categort:'Shirts',
+            image:'/images/p6.jpg',
+            price:33,
+            countInStock: 0,
+            brand:'Polo',
+            rating:4.8,
+            numReviews: 26,
+            description:'High quality product'
+        },
+    ],
+};
+
+export default data;
